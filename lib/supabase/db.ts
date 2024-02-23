7de4ca2d1e34f5746dcde39effc38ff4ce695456
achyutbenz19 <achyut.benz@gmail.com>
2024-02-22 19:52:29 -0500

    db

--- lib/supabase/db.ts
+++ lib/supabase/db.ts
@@ -1,26 +1,26 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import * as dotenv from 'dotenv'
-import * as schema from '../../migrations/schema'
-import { migrate } from 'drizzle-orm/postgres-js/migrator';
-dotenv.config({ path: '.env.local' });
+import * as dotenv from "dotenv";
+import * as schema from "../../migrations/schema";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+dotenv.config({ path: ".env" });
 
 if (!process.env.DATABASE_URL) {
-    console.log('🔴 Cannot find database url');
+  console.log("🔴 Cannot find database url");
 }
 
-const client = postgres(process.env.DATABASE_URL as string)
-const db = drizzle(client, { schema })
+const client = postgres(process.env.DATABASE_URL as string);
+const db = drizzle(client, { schema });
 
 const migrateDb = async () => {
-    try {
-        console.log('🟠 Migrating client');
-        await migrate(db, { migrationsFolder: 'migrations' });
-        console.log('🟢 Successfully Migrated');
-    } catch (error) {
-        console.log('🔴 Error Migrating client', error);
-    }
+  try {
+    console.log("🟠 Migrating client");
+    await migrate(db, { migrationsFolder: "migrations" });
+    console.log("🟢 Successfully Migrated");
+  } catch (error) {
+    console.log("🔴 Error Migrating client", error);
+  }
 };
 migrateDb();
 
-export default db
+export default db;

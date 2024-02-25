3fe0e4d67f05037c289927092753d8eca558c9a6
achyutbenz19 <achyut.benz@gmail.com>
2024-02-25 13:33:59 -0500

    auth beta

--- app/(landing)/_components/navbar.tsx
+++ app/(landing)/_components/navbar.tsx
@@ -4,9 +4,16 @@ import { cn } from "@/lib/utils";
 import React from "react";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 const Navbar = () => {
   const scrolled = useScrollTop();
+  const { isLoading, isAuthenticated } = useConvexAuth();
+
   return (
     <div
       className={cn(
@@ -21,6 +28,25 @@ const Navbar = () => {
         </span>
       </div>
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isLoading && !isAuthenticated && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get Wotion Free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <Button variant="ghost" size="sm" asChild>
+            <Link href="/documents">Enter Wotion</Link>
+            <UserButton afterSignOutUrl="/" />
+          </Button>
+        )}
         <ModeToggle />
       </div>
     </div>

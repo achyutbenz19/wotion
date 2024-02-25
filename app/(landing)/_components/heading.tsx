b0b5ef7b89a47faea9a95078df35767bc4cd03cb
achyutbenz19 <achyut.benz@gmail.com>
2024-02-25 14:11:03 -0500

    auth heading

--- app/(landing)/_components/heading.tsx
+++ app/(landing)/_components/heading.tsx
@@ -1,10 +1,14 @@
 "use client";
 
+import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
-import { ArrowRight } from "lucide-react";
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
+import { ArrowRight, Link } from "lucide-react";
 import React from "react";
 
 const Heading = () => {
+  const { isLoading, isAuthenticated } = useConvexAuth();
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -15,10 +19,27 @@ const Heading = () => {
         Wotion is the connected workspace where <br />
         better, faster work happens.
       </h3>
-      <Button>
-        Enter Wotion
-        <ArrowRight className="w-4 h-4 ml-2" />
-      </Button>
+      {isLoading && (
+        <div className="w-full items-center flex justify-center">
+          <Spinner size="lg"/>
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Wotion
+            <ArrowRight className="w-4 h-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get Wotion free
+            <ArrowRight className="w-4 h-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   );
 };

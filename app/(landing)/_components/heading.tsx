85fe0012253a11e8895c3b3158874149291b7416
achyutbenz19 <achyut.benz@gmail.com>
2024-02-25 11:48:08 -0500

    heading landing

---
+++ app/(landing)/_components/heading.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+        Your Ideas, Documents, & Plans - Unified. Welcome to{" "}
+        <span className="underline font-extrabold">Wotion</span>
+      </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        Wotion is the connected workspace where <br />
+        better, faster work happens.
+      </h3>
+      <Button>
+        Enter Wotion
+        <ArrowRight  className="w-4 h-4 ml-2" />
+      </Button>
+    </div>
+  );
+};
+
+export default Heading;

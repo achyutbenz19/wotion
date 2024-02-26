7c6c6e207d2b2cbaf85e5563314add79589b0c12
achyutbenz19 <achyut.benz@gmail.com>
2024-02-25 20:10:27 -0500

    documents page beta

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { Spinner } from "@/components/spinner";
+import { useConvexAuth } from "convex/react";
+import { redirect } from "next/navigation";
+import Navigation from "./_components/navigation";
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const { isLoading, isAuthenticated } = useConvexAuth();
+  if (isLoading)
+    return (
+      <div className="flex h-full items-center justify-center">
+        <Spinner size="lg" />
+      </div>
+    );
+
+  if (!isAuthenticated) {
+    return redirect("/");
+  }
+  return (
+    <div className="h-full flex dark:bg-[#1F1F1F]">
+      <Navigation />
+      <main className="flex-1 h-full overflow-y-auto">
+        {/* <SearchCommand /> */}
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default MainLayout;

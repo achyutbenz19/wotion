ec5e572b698b21ebf18f504141e92c0bef16630d
achyutbenz19 <achyut.benz@gmail.com>
2024-02-26 15:17:35 -0500

    skeleton

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -20,7 +20,7 @@ export const Navbar = ({ isCollapsed, onReset }: NavbarProps) => {
       <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
         <Title.Skeleton />
         <div className="flex items-center gap-x-2">
-          {/* <Menu.Skeleton /> */}
+          <Menu.Skeleton />
         </div>
       </nav>
     );
